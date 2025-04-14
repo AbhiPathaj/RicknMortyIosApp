27be101643fd2adf4b01c34303bd764954157d25
Abhishek Pathak <pathajAbhishek@gmail.com>
2025-04-14 10:22:05 +0530

    Initial Commit

---
+++ RickNMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickNMorty
+//
+//  Created by Abhishek Pathak on 14/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
